933de04417f4b8c7ad3e12b4a2cf9a4849c743bc
iourt <appuebear@gmail.com>
2015-11-23 23:55:16 +0800

    Update android-install.js

--- scripts/android-install.js
+++ scripts/android-install.js
@@ -5,7 +5,8 @@ module.exports = function (context) {
         fs           = context.requireCordovaModule('fs'),
         shell        = context.requireCordovaModule('shelljs'),
         projectRoot  = context.opts.projectRoot,
-        ConfigParser = context.requireCordovaModule('cordova-common').ConfigParser,
+        //ConfigParser = context.requireCordovaModule('cordova-common').ConfigParser,
+        ConfigParser = context.requireCordovaModule('cordova-lib/src/configparser/ConfigParser'),
         config       = new ConfigParser(path.join(context.opts.projectRoot, "config.xml")),
         packageName = config.android_packageName() || config.packageName();
 
